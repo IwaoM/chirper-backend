9762372e50da34858f75089c7a11ee0d6d9469b9
“IwaoM” <iwao27@gmail.com>
2022-12-06 16:19:11 +0100

    added access to SQL data, database structure & mock data

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # chirper-backend
 
 Back-end repo for Chirper, a WIP basic Twitter-like app
+
+## Requirements
+
+- MySQL should be installed (all data is locally stored)
+
+## Setup
+
+- `configTemplate.json` should be renamed to `config.json` and its values changed with your own MySQL credentials
--- app.js
+++ app.js
@@ -1,6 +1,44 @@
 const express = require("express");
+const mysql = require("mysql");
+const fs = require("node:fs");
+const path = require("node:path");
+const { mysqlUsername, mysqlPassword } = require("./config.json");
 
+//* connect to MySQL
+const con = mysql.createConnection({
+  multipleStatements: true,
+  host: "localhost",
+  user: mysqlUsername,
+  password: mysqlPassword
+});
+
+// input script
+const sqlScriptDir = path.join(__dirname, "initDatabase.sql");
+const sqlScript = fs.readFileSync(sqlScriptDir, { encoding:"utf8", flag:"r" });
+
+con.connect((error) => {
+  if (error) {
+    throw error;
+  }
+  console.log("Connected to MySQL");
+  con.query(sqlScript, function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("Script executed");
+  });
+});
+
+//* construct app
 const app = express();
+
+// allow requests from any origin
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
 
 app.use((req, res, next) => {
   res.status(200);

f2ffa165ea0931d1d037ba878126dc3116f6f3e7
“IwaoM” <iwao27@gmail.com>
2022-12-14 20:10:55 +0100

    feat: make database reinitialization optional when starting the server

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mysql = require("mysql");
-const { mysqlUsername, mysqlPassword } = require("./config.json");
 const fs = require("node:fs");
 const path = require("node:path");
+const readline = require("readline");
+const { mysqlUsername, mysqlPassword } = require("./config.json");
 
 const connection = mysql.createConnection({
   multipleStatements: true,
@@ -11,18 +12,36 @@ const connection = mysql.createConnection({
 });
 
 // input script
-const sqlScriptDir = path.join(__dirname, "initDatabase.sql");
-const sqlScript = fs.readFileSync(sqlScriptDir, { encoding:"utf8", flag:"r" });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 
 connection.connect(error => {
   if (error) {
     throw error;
   }
-  connection.query(sqlScript, function (err) {
-    if (err) {
-      throw err;
+  rl.question("Reset database? (y/n): ", function (answer) {
+    if (answer.toLowerCase() === "yes" || answer.toLowerCase() === "y") {
+      const sqlScriptDir = path.join(__dirname, "initDatabase.sql");
+      const sqlScript = fs.readFileSync(sqlScriptDir, { encoding:"utf8", flag:"r" });
+      connection.query(sqlScript, function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log("Database init script executed");
+      });
+    } else {
+      connection.query("USE chirper", function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log("Database init script skipped");
+      });
     }
-    console.log("Database init script executed");
+    rl.close();
+    console.log("Connection to MySQL ready");
   });
 });
 
